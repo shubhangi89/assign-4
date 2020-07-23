b6564597e9a9ec6c184d2158bec84313ffebebda
Shubhangi Aggarwal <shubhagg1206@gmail.com>
2020-07-23 16:33:13 +0530

    commit

--- module5-solution/js/script.js
+++ module5-solution/js/script.js
@@ -15,7 +15,7 @@ var dc = {};
 
 var homeHtmlUrl = "snippets/home-snippet.html";
 var allCategoriesUrl =
-https://davids-restaurant.herokuapp.com/categories.json  // "https://davids-restaurant.herokuapp.com/categories.json";
+  "https://davids-restaurant.herokuapp.com/categories.json"  // "https://davids-restaurant.herokuapp.com/categories.json";
 var categoriesTitleHtml = "snippets/categories-title-snippet.html";
 var categoryHtml = "snippets/category-snippet.html";
 var menuItemsUrl =
